7ecd95700ced29f03623dd91ec64ab96e888cc32
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-02-05 17:55:28 +0100

    fixed darkmode button

--- src/app/components/DarkModeButton.tsx
+++ src/app/components/DarkModeButton.tsx
@@ -1,43 +1,49 @@
 "use client";
 
-import {BsFillMoonFill, BsFillSunFill} from 'react-icons/bs'
-import { useEffect, useState } from 'react'
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import { useEffect, useState } from "react";
 
-import { useTheme } from 'next-themes'
+import { useTheme } from "next-themes";
 
 const DarkModeButton = () => {
-  const { theme, setTheme, systemTheme } = useTheme()
-  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
   if (!mounted) {
-    return null
+    return null;
   }
 
-  const currentTheme = theme === 'system' ? systemTheme : theme
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
-    <div className="flex items-center justify-end p-6 animate-[slide-down-fade_1.8s_ease-in-out] ">
-      <select 
-        className="px-4 leading-normal bg-transparent border border-gray-300 rounded-lg appearance-none focus:outline-none focus:shadow-outline" 
-        value={theme} 
-        onChange={e => setTheme(e.target.value)}
+    <div className="flex animate-[slide-down-fade_1.8s_ease-in-out] items-center justify-end p-6 ">
+      <select
+        className="focus:shadow-outline appearance-none rounded-lg border border-gray-300 bg-transparent px-4 leading-normal text-[#F9D8DD] focus:outline-none"
+        value={theme}
+        onChange={(e) => setTheme(e.target.value)}
       >
-        <option className="text-gray-700" value="system">System</option>
-        <option className="text-gray-700" value="dark">Dark</option>
-        <option className="text-slate-50" value="light">Light</option>
+        <option className="text-[#F9D8DD] " value="system">
+          System
+        </option>
+        <option className="text-[#F9D8DD] " value="dark">
+          Dark
+        </option>
+        <option className="text-[#F9D8DD] " value="light">
+          Light
+        </option>
       </select>
-      <button 
-        className="w-8 h-8 ml-5 cursor-pointer " 
-        onClick={()=> setTheme(currentTheme === 'dark' ? "light" : "dark")}
+      <button
+        className="ml-5 h-8 w-8 cursor-pointer text-[#F9D8DD] "
+        onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
       >
-        {currentTheme === 'dark' ? (<BsFillSunFill  />) : (<BsFillMoonFill  />)}
+        {currentTheme === "dark" ? <BsFillSunFill /> : <BsFillMoonFill />}
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default DarkModeButton
+export default DarkModeButton;
